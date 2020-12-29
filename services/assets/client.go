84dd6f68bec66860b3372245d6d5713a4b17a6c4
hewigovens <360470+hewigovens@users.noreply.github.com>
2020-12-29 19:03:56 +0900

    filter from assets list

--- services/assets/client.go
+++ services/assets/client.go
@@ -1,16 +1,17 @@
 package assets
 
 import (
+	"time"
+
 	"github.com/trustwallet/blockatlas/pkg/blockatlas"
 	"github.com/trustwallet/golibs/coin"
-	"time"
 )
 
 const (
 	AssetsURL = "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/"
 )
 
-func fetchValidatorsInfo(coin coin.Coin) (AssetValidators, error) {
+func GetchValidatorsInfo(coin coin.Coin) (AssetValidators, error) {
 	var results AssetValidators
 	request := blockatlas.InitClient(AssetsURL + coin.Handle)
 	err := request.GetWithCache(&results, "validators/list.json", nil, time.Hour*1)
